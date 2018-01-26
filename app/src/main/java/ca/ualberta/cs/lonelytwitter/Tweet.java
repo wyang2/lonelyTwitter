79a0745300ac065c750fcfa84d63240c3224e45a
Wenhan Yang <w.yangcanada@gmail.com>
2018-01-25 18:51:40 -0700

    lab3

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -44,4 +44,8 @@ public abstract class Tweet implements Tweetable {
     }
 
     public abstract Boolean isImportant();
+
+    public String toString(){
+        return date.toString()+"|" + message;
+    }
 }
